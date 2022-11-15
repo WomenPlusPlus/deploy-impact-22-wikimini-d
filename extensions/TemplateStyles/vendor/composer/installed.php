32133c3a93090e547abf21840d6896b4303f4929
Irina Borisova <i.a.borisova@gmail.com>
2022-11-15 12:56:40 +0100

    template styles

---
+++ extensions/TemplateStyles/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c3278582735b3d6ad17450fb1eb499146a07c42e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c3278582735b3d6ad17450fb1eb499146a07c42e',
+    ),
+    'wikimedia/at-ease' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8ebaa7bb7c8a8395481a05f6dc4deaceab11c33',
+    ),
+    'wikimedia/css-sanitizer' => 
+    array (
+      'pretty_version' => 'v3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '718e2a79fc3fec4c7a43193b5e2ac3bae6756ee4',
+    ),
+    'wikimedia/utfnormal' => 
+    array (
+      'pretty_version' => '3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e690d29487a6ee346bcf4cbf5a6fd89170fa2306',
+    ),
+  ),
+);
